f136fbaf0bb6b740235082aa2dc492adb8c3fc95
StXoTTaBbl4 <arumskiy@gmail.com>
2023-04-17 15:42:01 +0300

    work in progress

--- src/main/java/com/example/javalab2/functions/FirstSystemOfFunctions.java
+++ src/main/java/com/example/javalab2/functions/FirstSystemOfFunctions.java
@@ -2,24 +2,27 @@ package com.example.javalab2.functions;
 
 public class FirstSystemOfFunctions {
 
-    public double calculateFirst(double x){
-        return x;
+    static double x_0 = 1;
+    static double y_0 = 2;
+
+    public double calculateFirst(double x, double y){
+        return 4 - Math.pow(x,2) - Math.pow(y,2);
     }
-    public double calculateSecond(double x){
-        return x;
+    public double calculateSecond(double x, double y){
+        return 3*Math.pow(x,2) - y;
     }
 
-    public double calculateFirst_FirstDerivativeX(double x){
-        return x;
+    public double calculateDeltaX(double y){
+        return (-1-4*y)/2;
     }
-    public double calculateFirst_FirstDerivativeY(double x){
-        return x;
+    public double calculateDeltaY(double x){
+        return (-1-2*x)/4;
     }
-    public double calculateSecond_FirstDerivativeX(double x){
-        return x;
-    }
-    public double calculateSecond_FirstDerivativeY(double x){
-        return x;
-    }
+//    public double calculateSecond_FirstDerivativeX(double x){
+//        return x;
+//    }
+//    public double calculateSecond_FirstDerivativeY(double x){
+//        return x;
+//    }
 
 }
